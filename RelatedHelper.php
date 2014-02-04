d9dbe9e0bacda138743f6cee0eec5629645d104e
Christoph Wiechert <wio@psitrax.de>
2014-02-04 14:47:24 +0100

    Auto fix broken VIEW (fixes #1)

--- RelatedHelper.php
+++ RelatedHelper.php
@@ -15,6 +15,17 @@ namespace Psi\News4ward;
 class RelatedHelper extends \Controller 
 {
 
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	protected $viewCreateQry = "CREATE OR REPLACE VIEW `tl_news4ward_articleWithTags` AS
+  SELECT tl_news4ward_article.*, GROUP_CONCAT(tag) AS tags
+  FROM tl_news4ward_article
+  LEFT OUTER JOIN tl_news4ward_tag ON (tl_news4ward_tag.pid = tl_news4ward_article.id)
+  GROUP BY tl_news4ward_article.id";
+
 	/**
 	 * Handle database update
 	 * its a callback, executed within install-tool
@@ -23,6 +34,7 @@ class RelatedHelper extends \Controller
 	 */
 	public function sqlCompileCommands($arrData)
 	{
+		$this->import('Database');
 		$arrAllModules = scan(TL_ROOT . '/system/modules');
 
 		// remove DROP statment for tl_news4ward_articleWithTags if news4ward_tags is installed
@@ -46,17 +58,28 @@ class RelatedHelper extends \Controller
 		}
 
 		// if news4ward_tags is installed tell contao to CREATE VIEW if not already done
-		$this->import('Database');
 		if(in_array('news4ward_tags',$arrAllModules) && !$this->Database->tableExists('tl_news4ward_articleWithTags'))
 		{
-			$arrData['CREATE'][] = "CREATE OR REPLACE VIEW `tl_news4ward_articleWithTags` AS
-  SELECT tl_news4ward_article.*, GROUP_CONCAT(tag) AS tags
-  FROM tl_news4ward_article
-  LEFT OUTER JOIN tl_news4ward_tag ON (tl_news4ward_tag.pid = tl_news4ward_article.id)
-  GROUP BY tl_news4ward_article.id";
+			$arrData['CREATE'][] = $this->viewCreateQry;
 		}
 
 		return $arrData;
+	}
+
+
+	/**
+	 * Repeair broken VIEW on demand
+	 */
+	public function fixView()
+	{
+		\BackendUser::getInstance(); // kid php destructor: we need to destruct BackendUser *before* Database
+		$this->import('Database');
+		if(!$this->Database->tableExists('tl_news4ward_articleWithTags')) return;
+		try {
+			$this->Database->query('SHOW INDEXES FROM `tl_news4ward_articleWithTags`');
+		} catch(\Exception $e) {
+			$this->Database->query($this->viewCreateQry);
+		}
 	}
 }
 
--- config/config.php
+++ config/config.php
@@ -1,4 +1,4 @@
-<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
+<?php
 
 /**
  * News4ward
@@ -17,3 +17,9 @@ $GLOBALS['FE_MOD']['news4ward']['news4wardRelated'] = '\News4ward\Module\Related
 
 // Hook for updateDatabase to create the view if news4ward_tags is installed
 $GLOBALS['TL_HOOKS']['sqlCompileCommands'][] = array('\News4ward\RelatedHelper','sqlCompileCommands');
+
+
+if(TL_MODE == 'BE' && (\Input::get('do') == 'composer' && \Input::get('update') == 'database' || \Environment::get('request') == 'contao/install.php'))
+{
+	$GLOBALS['TL_HOOKS']['initializeSystem'][] = array('\News4ward\RelatedHelper','fixView');
+}
